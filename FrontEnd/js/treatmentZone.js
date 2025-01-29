34a0b5b7ba2fd9b8ecac289ca0e02b0216de9f47
dSeguraSanValero <a27238@svalero.com>
2025-01-29 16:02:07 +0100

    Creamos el menú en la zona de tratamiento

--- FrontEnd/js/treatmentZone.js
+++ FrontEnd/js/treatmentZone.js
@@ -8,20 +8,39 @@ window.onload = function() {
 
     const patientData = sessionStorage.getItem("patientData");
 
-    if (patientData) {
-        const patient = JSON.parse(patientData);
+    const patient = JSON.parse(patientData);
 
-        const container = document.getElementById('patientName-container');
+    const container = document.getElementById('patientName-container');
 
-        const patientDiv = document.createElement('div');
+    const patientDiv = document.createElement('div');
 
-        patientDiv.innerHTML = `
-            <p><strong>Paciente: ${patient.name} ${patient.firstSurname} ${patient.secondSurname}.</strong></p>
-        `;
+    patientDiv.innerHTML = `
+        <p><strong>Patient: ${patient.name} ${patient.firstSurname} ${patient.secondSurname}</strong></p>
+    `;
 
-        container.appendChild(patientDiv);
-    } else {
-        console.log("No se encontraron datos del paciente en el sessionStorage.");
-    }
+    container.appendChild(patientDiv);
+
+    showSection('date-card');   
+
 };
+
+function showSection(sectionClass) {
+    document.querySelectorAll('.card').forEach(section => {
+
+        section.style.opacity = '0';
+        section.style.transform = 'translateY(20px)';
+        section.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+
+        section.classList.remove('active');
+
+        setTimeout(() => {
+            section.style.opacity = '1';
+            section.style.transform = 'translateY(0)';
+        }, 300);
+    });
+    
+    document.querySelectorAll(`.${sectionClass}`).forEach(section => {
+        section.classList.add('active');
+    });
+}
 
